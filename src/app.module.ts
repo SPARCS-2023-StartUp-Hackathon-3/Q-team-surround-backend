52fca36bfb62c9f5861a5deb5a8439caf24c6433
Seongtae <stae1102@gmail.com>
2023-01-27 21:53:33 +0900

    :sparkles: feat: add prisma service to the nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
