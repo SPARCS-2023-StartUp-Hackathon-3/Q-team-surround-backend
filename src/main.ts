d4be2a201e81dd08e950402720d281c984255aec
Seongtae <stae1102@gmail.com>
2023-01-28 00:53:45 +0900

    :art: feat: add helmet, set prefix

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { setupSwagger } from '../libs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   setupSwagger(app);
+  app.setGlobalPrefix('api');
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
