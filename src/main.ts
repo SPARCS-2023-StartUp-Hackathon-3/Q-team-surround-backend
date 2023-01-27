7d00511c80e998bbc78b4e0211115cbc344d1948
Seongtae <stae1102@gmail.com>
2023-01-28 00:40:24 +0900

    :heavy_plus_sign: chore: add swagger setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { setupSwagger } from '../libs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  setupSwagger(app);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
