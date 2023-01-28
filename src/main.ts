74bd697792d80cec228cb0b3bfb7d4e313cb35cd
Seongtae <stae1102@gmail.com>
2023-01-28 09:00:17 +0900

    :zap: fix: apply prefix to swagger

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  setupSwagger(app);
   app.setGlobalPrefix('api');
+  setupSwagger(app);
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
