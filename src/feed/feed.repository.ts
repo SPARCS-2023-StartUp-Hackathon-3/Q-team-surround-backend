3b95b57ecc447254b634f154b7138698fa05d0e8
Seongtae <stae1102@gmail.com>
2023-01-29 11:03:05 +0900

    :zap: feat: add exceptions

--- src/feed/feed.repository.ts
+++ src/feed/feed.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateFeedRequestDto } from './dtos/feed-request.dto';
 
@@ -8,40 +8,47 @@ export class FeedRepository {
 
   async createFeed(userId: number, createFeedRequestDto: CreateFeedRequestDto) {
     const { content, songId } = createFeedRequestDto;
-    console.log(songId);
-    if (songId) {
-      return await this.prisma.feed.create({
-        data: {
-          content,
-          UserId: userId,
-          Song: {
-            connect: {
-              id: songId,
+    try {
+      if (songId) {
+        return await this.prisma.feed.create({
+          data: {
+            content,
+            UserId: userId,
+            Song: {
+              connect: {
+                id: songId,
+              },
             },
           },
+          include: {
+            Song: true,
+          },
+        });
+      } else {
+        return await this.prisma.feed.create({
+          data: {
+            content,
+            UserId: userId,
+          },
+        });
+      }
+    } catch (err) {
+      throw new BadRequestException('유효하지 않은 요청입니다.');
+    }
+  }
+
+  async findFeeds(userId: number) {
+    try {
+      return await this.prisma.feed.findMany({
+        where: {
+          UserId: userId,
         },
         include: {
           Song: true,
         },
       });
-    } else {
-      return await this.prisma.feed.create({
-        data: {
-          content,
-          UserId: userId,
-        },
-      });
+    } catch (err) {
+      throw new BadRequestException('유저 아이디를 확인해주세요.');
     }
-  }
-
-  async findFeeds(userId: number) {
-    return await this.prisma.feed.findMany({
-      where: {
-        UserId: userId,
-      },
-      include: {
-        Song: true,
-      },
-    });
   }
 }
